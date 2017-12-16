3fe22a7504df4f9c28969a4504f7ad75b9f35df9
Alina Moskieva <alina.moskieva@flatstack.com>
2017-12-16 10:39:32 +0300

    bench test with random text

--- pgbench/bench_test.sql
+++ pgbench/bench_test.sql
@@ -1,7 +1,8 @@
 \set limit random(1, 32)
 \set offset random(1, 32)
--- not working because set key accepts only integer value
-\set content select array_to_string(array(select unnest(string_to_array('Before the king could rise, his frightened horse, although lame, had galloped away. The king looked, and saw that his soldiers were beaten, and that the battle was everywhere going against him',' ')) limit :limit offset :offset),' & ')
 
-
-select * from news where to_tsvector(content) @@ to_tsquery(:content)
+select * from news where to_tsvector(content) @@ to_tsquery(
+ array_to_string(array(select unnest
+                      (string_to_array('Before the king could rise, his frightened horse, although lame, had galloped away. The king looked, and saw that his soldiers were beaten, and that the battle was everywhere going against him',' ')) 
+                      limit :limit offset :offset),' & ')::text 
+   )
--- pgbench/results.sql
+++ pgbench/results.sql
@@ -33,3 +33,17 @@ tps = 917.032058 (including connections establishing)
 tps = 917.075134 (excluding connections establishing)
 
 
+pgbench -T 300 -l -f /Users/moskieva/University/SMDB/pgbench/bench_test_for_fts.sql SMDB
+starting vacuum...end.
+transaction type: /Users/moskieva/University/SMDB/pgbench/bench_test_for_fts.sql
+scaling factor: 1
+query mode: simple
+number of clients: 1
+number of threads: 1
+duration: 300 s
+number of transactions actually processed: 1043420
+latency average = 0.288 ms
+tps = 3478.049468 (including connections establishing)
+tps = 3478.098498 (excluding connections establishing)
+
+
